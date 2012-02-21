b873c86bca2d105ea9b3aa13ef3dfe15cb5150ec
Russ Anderson <russ.anderson@yellglobal.com>
2012-02-21 09:15:28 +0000

    vibration bits

--- cornflake/CHViewController.m
+++ cornflake/CHViewController.m
@@ -22,6 +22,7 @@
     AudioServicesCreateSystemSoundID(soundFileURLRef, &soundID);
     AudioServicesPlaySystemSound(soundID);
 
+    AudioServicesPlaySystemSound (kSystemSoundID_Vibrate);
 
     [UIView animateWithDuration:1
         animations:^{
